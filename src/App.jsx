66e95b61020c4c78419175f3588469d7ea762c02
shui6540 <priyanshiagarwal4851@gmail.com>
2024-09-27 01:10:15 +0530

    corrected

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,6 @@ import PopulationChart from './components/PopulationChart';
 import './i18n';
 import CategoryGrid from './components/CategoryGrid';  // Import CategoryGrid
 import AboutUs from './components/AboutUs';
-<<<<<<< HEAD
-=======
-
->>>>>>> 1dd456101f9e418cf107500b21cf439301c74831
 function App() {
   return (
     <Router>
